f3294cd689d0a1187acab9aaa2e980ad56991b6a
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-10-15 12:12:13 -0400

    Implement a Linux backend
      * The linux backend can't use `sysctl` to get the stats, so we need
        to parse the files directly.
    
      * This information is not presented as files in FreeBSD.
    
      * For linux we can test the impure part of this backend against
        a mock directory of the zfs objset files normally found in
        /proc/spl/kstat/zfs/*/objset* .
    
    Take HashMap reference instead of moving.
    
      * snapshot_from_hash_map:
        - Take a reference to a HashMap with a lifetime.
      * SnapshotParseData:
        - Add lifetime.
        - Use `&'a str` instead of `String`.  It is `to_string`ed in
          `snapshot_from_hash_map`, but this way we only copy once.
    
    Some cleanup and parsing tests...
    
      * Added tests for parsing Snapshots from spl/procfs file content.
    
    Iterator strategy solved.
    
      * Get all the paths to the objsets up front.
      * Snapshot iter is then a simple flat iterator over these paths.

--- src/app/linux.rs
+++ src/app/linux.rs
@@ -1,13 +1,104 @@
 // vim: tw=80
-use std::error::Error;
+use std::{
+    error::Error,
+    fs,
+    io,
+    fmt,
+    path::{Path, PathBuf},
+    collections::{HashSet, HashMap},
+};
+use super::{
+    Snapshot,
+};
 
-use super::Snapshot;
 
-pub(super) struct SnapshotIter {}
+/// SnapshotIter allocates a vector of the paths to objsets
+/// under the desired pools on initialization.  Reading the
+/// objsets into Snapshots is lazy.
+pub(super) struct SnapshotIter {
+    idx: usize,
+    objsets: Vec<PathBuf>
+}
+
+/// This is the default filepath for zfs stats using ZoL.
+const DEFAULT_ZFS_STATS_PATH: &'static str = "/proc/spl/kstat/zfs";
 
 impl SnapshotIter {
-    pub(crate) fn new(_pool: Option<&str>) -> Result<Self, Box<dyn Error>> {
-        unimplemented!()
+    /// Simply use the default path to the stats files in procfs.  A value of `None`
+    /// for `pool` will get statistics for all pools.
+    pub(crate) fn new<'a>(pool: Option<&str>) -> Result<Self, Box<dyn Error>> {
+        Self::new_from_basepath(DEFAULT_ZFS_STATS_PATH, pool)
+    }
+
+    // Useful for testing on a mock directory.
+    fn new_from_basepath(
+        basepath: &str,
+        pool: Option<&str>)
+        -> Result<SnapshotIter, Box<dyn Error>> {
+        let objsets = Self::required_pools_from_basepath(basepath, pool)?
+            .into_iter()
+            .flat_map(Self::enumerate_pool)
+            .collect::<Vec<PathBuf>>();
+        Ok(SnapshotIter{ idx: 0, objsets })
+    }
+
+    // Get all objset paths for a given pool.
+    // At this point all the paths should exist, because they have been
+    // checked by `required_pools_from_basepath`.  Should this somehow be
+    // expressed as an invariant?
+    fn enumerate_pool(pool: PathBuf) -> Vec<PathBuf> {
+        let is_dataset = |entry: &fs::DirEntry| {
+            let path = entry.path();
+            let name = entry.file_name();
+            let name = name.to_str().unwrap_or("");
+            path.is_file() && name.starts_with("objset")
+        };
+        fs::read_dir(pool).map_or(vec![], |dir| {
+            dir.filter_map(|e| e.map_or(None, |entry| {
+                if is_dataset(&entry) {
+                    Some(entry.path().to_path_buf())
+                } else {
+                    None
+                }
+            })).collect()
+        })
+    }
+
+    fn required_pools_from_basepath(
+        basepath: &str,
+        pool: Option<&str>)
+        -> Result<HashSet<PathBuf>, Box<dyn Error>> {
+        let zfs_stats_path = PathBuf::from(basepath);
+        let mut pools = SnapshotIter::get_pools(zfs_stats_path.as_path())
+            .map_or_else(|err| Err(Box::new(err)), |x| Ok(x))?;
+        if let Some(pool) = pool {
+            let pool = PathBuf::from(pool);
+            pools.retain(|p| p == &pool);
+            if pools.is_empty() {
+                let name = pool.file_name()
+                    .map(|ostr| ostr.to_str().unwrap_or(""))
+                    .unwrap_or("").to_string();
+                return Err(Box::new(ZTopError::PoolDoesNotExist { pool: name }));
+            }
+        }
+        Ok(pools)
+    }
+
+    fn get_pools(zfs_stat_path: &Path) -> io::Result<HashSet<PathBuf>> {
+        fs::read_dir(zfs_stat_path).map(|dir| {
+            dir.filter_map(|entry| {
+                if let Ok(entry) = entry {
+                    let path = entry.path();
+                    if path.is_dir() {
+                        Some(path.to_path_buf())
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            }).collect::<HashSet<PathBuf>>()
+        })
     }
 }
 
@@ -15,6 +106,171 @@ impl Iterator for SnapshotIter {
     type Item = Result<Snapshot, Box<dyn Error>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        unimplemented!()
+        self.idx += 1;
+        self.objsets.get(self.idx - 1)
+            .map(|objset| {
+                fs::read_to_string(objset)
+                    .map_err(|err| Box::new(err) as Box<dyn Error>)
+                    .and_then(|data| {
+                        parse_snapshot(data.as_ref())
+                            .map_err(|err| Box::new(err) as Box<dyn Error>)
+                    })
+            })
+    }
+}
+
+fn snapshot_from_hash_map<'a>(stats: &'a HashMap<&str, SnapshotParseData>) -> Option<Snapshot> {
+    use SnapshotParseData::{Name,Number};
+    let get_name = |data: &'a SnapshotParseData| match data {
+        Name(name) => Some(name),
+        _ => None,
+    };
+    let get_number = |data: &'a SnapshotParseData| match data {
+        Number(n) => Some(*n),
+        _ => None,
+    };
+    let name = stats.get("dataset_name").and_then(get_name)?.to_string();
+    let nunlinked = stats.get("nunlinked").and_then(get_number)?;
+    let nunlinks = stats.get("nunlinks").and_then(get_number)?;
+    let nread = stats.get("nread").and_then(get_number)?;
+    let reads = stats.get("reads").and_then(get_number)?;
+    let nwritten = stats.get("nwritten").and_then(get_number)?;
+    let writes = stats.get("writes").and_then(get_number)?;
+    Some(Snapshot { name, nunlinked, nunlinks, nread, reads, nwritten, writes })
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum ZTopError {
+    PoolDoesNotExist{pool: String},
+    SnapshotParseError{src: String},
+}
+
+impl Error for ZTopError {}
+
+impl fmt::Display for ZTopError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ZTopError::SnapshotParseError{src} =>
+                write!(f, "Failed to parse Snapshot from string:\n {}", src),
+            ZTopError::PoolDoesNotExist{pool} =>
+                write!(f, "ZFS pool does not exist: {}", pool),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum SnapshotParseData<'a> {
+    Name(&'a str),
+    Number(u64),
+}
+
+fn parse_snapshot(s: &str) -> Result<Snapshot, ZTopError> {
+    use SnapshotParseData::{Name,Number};
+    let mut stats = HashMap::new();
+    for row in s.split("\n") {
+        let  fields: Vec<_> = row.split_whitespace().collect();
+        match fields[..] {
+            [name, _, data] if name != "name" => {
+                let data = data.parse::<u64>().map_or_else(
+                    |_| Name(data),
+                    Number
+                );
+                stats.insert(name, data);
+            },
+            _ => {},
+        }
+    }
+    if let Some(snap) = snapshot_from_hash_map(&stats) {
+        Ok(snap)
+    } else {
+        Err(ZTopError::SnapshotParseError{ src: s.to_owned()})
+    }
+}
+
+#[cfg(test)]
+mod t {
+    mod snapshot_parsing {
+        use super::super::*;
+
+        const OBJSET: [&str; 9] = [
+            "26 1 0x01 7 2160 15278395714 17176723400350",
+            "name                            type data",
+            "dataset_name                    7    tank",
+            "writes                          4    14",
+            "nwritten                        4    256",
+            "reads                           4    100",
+            "nread                           4    1024",
+            "nunlinks                        4    1",
+            "nunlinked                       4    4",
+        ];
+
+        /// Empty string is insufficient data
+        #[test]
+        fn empty() {
+            assert!(parse_snapshot("").is_err());
+        }
+
+        /// Parses full objset content including the header
+        #[test]
+        fn full_objset() {
+            let objset = OBJSET.iter().map(|&x| x).collect::<Vec<&str>>().join("\n");
+            let expected: Result<Snapshot, ZTopError>  = Ok(Snapshot {
+                name: "tank".to_owned(),
+                nunlinked: 4,
+                nunlinks: 1,
+                nread: 1024,
+                reads: 100,
+                nwritten: 256,
+                writes: 14,
+            });
+            assert_eq!(expected, parse_snapshot(&objset));
+        }
+
+        /// Parses without the header lines (just in case)
+        #[test]
+        fn without_header() {
+            let objset = &OBJSET[2..9].iter().map(|&x| x).collect::<Vec<&str>>().join("\n");
+            let expected: Result<Snapshot, ZTopError>  = Ok(Snapshot {
+                name: "tank".to_owned(),
+                nunlinked: 4,
+                nunlinks: 1,
+                nread: 1024,
+                reads: 100,
+                nwritten: 256,
+                writes: 14,
+            });
+            assert_eq!(expected, parse_snapshot(&objset));
+        }
+
+        /// The order of the file/data doesn't matter.
+        #[test]
+        fn out_of_order() {
+            let mut objset = OBJSET.iter().map(|&x| x).collect::<Vec<&str>>();
+            objset.reverse();
+            let objset = objset.join("\n");
+            let expected: Result<Snapshot, ZTopError>  = Ok(Snapshot {
+                name: "tank".to_owned(),
+                nunlinked: 4,
+                nunlinks: 1,
+                nread: 1024,
+                reads: 100,
+                nwritten: 256,
+                writes: 14,
+            });
+            assert_eq!(expected, parse_snapshot(&objset));
+        }
+
+        /// Missing field fails parsing
+        ///
+        /// We should probably have a test for each field missing?
+        #[test]
+        fn missing_fields() {
+            // Leave off the end field.
+            let objset = &OBJSET[2..8].iter().map(|&x| x).collect::<Vec<&str>>().join("\n");
+            assert!(parse_snapshot(objset).is_err());
+            // Strip the dataset_name field.
+            let objset = &OBJSET[3..9].iter().map(|&x| x).collect::<Vec<&str>>().join("\n");
+            assert!(parse_snapshot(objset).is_err());
+        }
     }
 }
